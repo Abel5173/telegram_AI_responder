53da4c48137a5c6a42d185f5d0391cb694cf326a
Abel <abelzeleke5173@gmail.com>
2025-07-01 01:11:21 +0300

    fix(tests): resolve flake8 linting errors in test_handlers.py

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -17,30 +17,40 @@ class TestHandlers(unittest.TestCase):
         message.from_user.id = self.owner_id_int
         handle_afk_command(message, self.bot, self.OWNER_AFK, self.owner_id_int)
         self.assertTrue(self.OWNER_AFK[0])
-        self.bot.reply_to.assert_called_with(message, "AFK mode enabled. I'll cover your DMs after 15 seconds if you don't reply.")
+        self.bot.reply_to.assert_called_with(
+            message, "AFK mode enabled. I'll cover your DMs after 15 seconds if you don't reply."
+        )
 
     def test_afk_command_non_owner(self):
         message = MagicMock()
         message.from_user.id = 999
         handle_afk_command(message, self.bot, self.OWNER_AFK, self.owner_id_int)
         self.assertTrue(self.OWNER_AFK[0])
-        self.bot.reply_to.assert_called_with(message, "Only the owner can use this command.")
+        self.bot.reply_to.assert_called_with(
+            message, "Only the owner can use this command."
+        )
 
     def test_back_command_owner(self):
         message = MagicMock()
         message.from_user.id = self.owner_id_int
         handle_back_command(message, self.bot, self.OWNER_AFK, self.owner_id_int)
         self.assertFalse(self.OWNER_AFK[0])
-        self.bot.reply_to.assert_called_with(message, "Welcome back! I won't auto-respond anymore.")
+        self.bot.reply_to.assert_called_with(
+            message, "Welcome back! I won't auto-respond anymore."
+        )
 
     def test_status_command(self):
         message = MagicMock()
         self.OWNER_AFK[0] = True
         handle_status_command(message, self.bot, self.OWNER_AFK)
-        self.bot.reply_to.assert_called_with(message, "Owner is currently AFK.")
+        self.bot.reply_to.assert_called_with(
+            message, "Owner is currently AFK."
+        )
         self.OWNER_AFK[0] = False
         handle_status_command(message, self.bot, self.OWNER_AFK)
-        self.bot.reply_to.assert_called_with(message, "Owner is currently online.")
+        self.bot.reply_to.assert_called_with(
+            message, "Owner is currently online."
+        )
 
 if __name__ == '__main__':
     unittest.main() 
